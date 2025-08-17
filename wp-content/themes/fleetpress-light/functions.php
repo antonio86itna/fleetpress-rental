96c8aa48fcb158b2b2a3e22de1efe4af8a1e9d96
antonio86itna <48077495+antonio86itna@users.noreply.github.com>
2025-08-17 17:39:08 +0200

    Create functions.php

---
+++ wp-content/themes/fleetpress-light/functions.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+add_action('after_setup_theme', function () {
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+});
+
+add_action('wp_enqueue_scripts', function () {
+  wp_enqueue_style('fpr-theme', get_stylesheet_uri(), [], '0.1.0');
+});
