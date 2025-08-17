0ae8bcbf6ccb9a63ce9e0d13b98162fa9136385e
antonio86itna <48077495+antonio86itna@users.noreply.github.com>
2025-08-17 17:40:15 +0200

    Create page-dashboard.php

---
+++ wp-content/themes/fleetpress-light/page-dashboard.php
@@ -0,0 +1,6 @@
+<?php /* Template Name: Customer Dashboard */ if (!defined('ABSPATH')) exit; get_header(); ?>
+<main class="fpr-dashboard">
+  <h1><?php _e('My Rentals', 'fpr'); ?></h1>
+  <div id="fpr-my-bookings"></div>
+</main>
+<?php get_footer(); ?>
